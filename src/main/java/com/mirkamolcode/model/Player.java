4a67b4d706bd374a9e679a66288489e307292891
Mirkamol <mirkamol.m.a@gmail.com>
2026-02-18 18:16:35 +0500

    feat(Player): create class with properties and methods

---
+++ src/main/java/com/mirkamolcode/model/Player.java
@@ -0,0 +1,46 @@
+package com.mirkamolcode.model;
+
+import java.util.Random;
+
+public class Player {
+    private String name;
+    private int waterTank;
+
+    public Player(String name) {
+        this.name = name;
+        this.waterTank = 100;
+    }
+
+    public int shoot() {
+        Random random = new Random();
+        return random.nextInt(20);
+    }
+
+    public void fillWaterGun(){
+        setWaterTank(100);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getWaterTank() {
+        return waterTank;
+    }
+
+    public void setWaterTank(int waterTank) {
+        this.waterTank = waterTank;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", waterTank=" + waterTank +
+                '}';
+    }
+}
