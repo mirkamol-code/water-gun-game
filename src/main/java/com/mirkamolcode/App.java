d081542f1bd2070a48ff96cdb1ce2196e799953b
Mirkamol <mirkamol.m.a@gmail.com>
2026-02-18 13:47:45 +0500

    feat(project): set up

---
+++ src/main/java/com/mirkamolcode/App.java
@@ -0,0 +1,13 @@
+package com.mirkamolcode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
