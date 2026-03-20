da65248512472cce2c5495f02c0764fc12e34936
mirkamol-code <mirkamol.m.a@gmail.com>
2026-03-20 23:07:05 +0500

    feat(project): build the game with imperative approach (#1)
    * feat(Player): create class with properties and methods
    
    * feat(Main): build a game
    
    * fix(OOP, SOLID): fixed issue after review
    
    * fix(Scanner): scaner is closed

---
+++ src/main/java/com/mirkamolcode/Main.java
@@ -0,0 +1,40 @@
+package com.mirkamolcode;
+
+import com.mirkamolcode.model.Player;
+import com.mirkamolcode.service.GameService;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GameService gameService = new GameService();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Water Gun Game!");
+        System.out.print("Enter Player 1's: ");
+        String firstPlayerName = scanner.nextLine();
+        System.out.print("Enter Player 2's: ");
+        String secondPlayerName = scanner.nextLine();
+
+        Player firstPlayer = new Player(firstPlayerName);
+        Player secondPlayer = new Player(secondPlayerName);
+
+        while (gameService.isGameOver(firstPlayer, secondPlayer)) {
+            System.out.println(firstPlayerName + "'s water: " + firstPlayer.getWaterTank());
+            System.out.println(secondPlayerName + "'s water: " + secondPlayer.getWaterTank());
+            if (gameService.isFirstPlayerTurn()) {
+                System.out.print(firstPlayerName + ", choose an action (1: Fill water gun, 2: Shoot): ");
+                var action = scanner.nextInt();
+                gameService.doAction(firstPlayer, secondPlayer, action);
+
+            } else {
+                System.out.print(secondPlayerName + ", choose an action (1: Fill water gun, 2: Shoot): ");
+                var action = scanner.nextInt();
+                gameService.doAction(secondPlayer, firstPlayer, action);
+
+            }
+        }
+        gameService.printWinner(firstPlayer, secondPlayer);
+        scanner.close();
+    }
+}
